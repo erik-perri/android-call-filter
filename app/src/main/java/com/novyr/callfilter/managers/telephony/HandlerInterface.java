c177b7ae0062ec62e481615ce97bc14ce756b5c0
Erik Perri <erik.perri@gmail.com>
2019-03-17 14:36:22 -0400

    Fixed typo

--- app/src/main/java/com/novyr/callfilter/managers/telephony/HandlerInterface.java
+++ app/src/main/java/com/novyr/callfilter/managers/telephony/HandlerInterface.java
@@ -2,7 +2,7 @@ package com.novyr.callfilter.managers.telephony;
 
 public interface HandlerInterface {
     /**
-     * @return Whether the call was sucessfully ended
+     * @return Whether the call was successfully ended
      */
     boolean endCall();
 }
