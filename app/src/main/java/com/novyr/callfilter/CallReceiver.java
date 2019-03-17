b89668327ef9488296fbb12c7238461025a67744
Erik Perri <erik.perri@gmail.com>
2019-03-16 22:35:19 -0400

    Added null check

--- app/src/main/java/com/novyr/callfilter/CallReceiver.java
+++ app/src/main/java/com/novyr/callfilter/CallReceiver.java
@@ -30,30 +30,31 @@ public class CallReceiver extends BroadcastReceiver {
         }
 
         String state = intent.getStringExtra(android.telephony.TelephonyManager.EXTRA_STATE);
-
-        if (state.equals(android.telephony.TelephonyManager.EXTRA_STATE_RINGING)) {
-            String number = intent.getStringExtra(android.telephony.TelephonyManager.EXTRA_INCOMING_NUMBER);
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P && number == null) {
-                // Since we request both READ_CALL_LOG and READ_PHONE_STATE permissions our onReceive
-                // will get called twice, the first has a null phone number.
-                // https://developer.android.com/reference/android/telephony/TelephonyManager#ACTION_PHONE_STATE_CHANGED
-                return;
-            }
-
-            HandlerInterface handler = TelephonyManager.findHandler(context);
-            String action = "allowed";
-            if (CallFilterApplication.shouldBlockCall(context, number)) {
-                if (handler.endCall()) {
-                    action = "blocked";
-                } else {
-                    action = "error";
-                }
-            }
-
-            LogEntry log = new LogEntry(new Date(), action, number);
-            log.save();
-
-            context.sendBroadcast(mBroadcastRefresh);
+        if (state == null || !state.equals(android.telephony.TelephonyManager.EXTRA_STATE_RINGING)) {
+            return;
         }
+
+        String number = intent.getStringExtra(android.telephony.TelephonyManager.EXTRA_INCOMING_NUMBER);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P && number == null) {
+            // Since we request both READ_CALL_LOG and READ_PHONE_STATE permissions our onReceive
+            // will get called twice, the first has a null phone number.
+            // https://developer.android.com/reference/android/telephony/TelephonyManager#ACTION_PHONE_STATE_CHANGED
+            return;
+        }
+
+        HandlerInterface handler = TelephonyManager.findHandler(context);
+        String action = "allowed";
+        if (CallFilterApplication.shouldBlockCall(context, number)) {
+            if (handler.endCall()) {
+                action = "blocked";
+            } else {
+                action = "error";
+            }
+        }
+
+        LogEntry log = new LogEntry(new Date(), action, number);
+        log.save();
+
+        context.sendBroadcast(mBroadcastRefresh);
     }
 }
