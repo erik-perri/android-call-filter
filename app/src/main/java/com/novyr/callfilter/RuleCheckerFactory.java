a9e0541dc8921404dbb688a4c6bec5a1b7182f71
Erik Perri <erik.perri@gmail.com>
2020-12-20 14:49:06 -0500

    fix: Stopped rule checker from being cached
    It was causing rules to not update in the services while the app was running

--- app/src/main/java/com/novyr/callfilter/RuleCheckerFactory.java
+++ app/src/main/java/com/novyr/callfilter/RuleCheckerFactory.java
@@ -7,21 +7,12 @@ import com.novyr.callfilter.db.entity.RuleEntity;
 import com.novyr.callfilter.rules.RuleHandlerManager;
 
 public class RuleCheckerFactory {
-    private static volatile RuleChecker INSTANCE;
-
     public static RuleChecker create(final Context context) {
-        if (INSTANCE == null) {
-            synchronized (RuleChecker.class) {
-                if (INSTANCE == null) {
-                    RuleRepository repo = ((CallFilterApplication) context.getApplicationContext())
-                            .getRuleRepository();
-                    INSTANCE = new RuleChecker(
-                            new RuleHandlerManager(new ContactFinder(context)),
-                            repo.findEnabled().toArray(new RuleEntity[0])
-                    );
-                }
-            }
-        }
-        return INSTANCE;
+        RuleRepository repo = ((CallFilterApplication) context.getApplicationContext())
+                .getRuleRepository();
+        return new RuleChecker(
+                new RuleHandlerManager(new ContactFinder(context)),
+                repo.findEnabled().toArray(new RuleEntity[0])
+        );
     }
 }
