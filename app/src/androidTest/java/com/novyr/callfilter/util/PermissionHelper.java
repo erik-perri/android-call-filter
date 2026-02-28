ba2f65bdd9b43eb36f3b985c187ebdb35c7aeeff
Erik Perri <erik.perri@gmail.com>
2026-02-28 16:07:11 -0500

    test: fixed call screening assignment across api versions

--- app/src/androidTest/java/com/novyr/callfilter/util/PermissionHelper.java
+++ app/src/androidTest/java/com/novyr/callfilter/util/PermissionHelper.java
@@ -39,29 +39,43 @@ public class PermissionHelper {
             return;
         }
 
-        // The dialog is a ListView inside com.google.android.permissioncontroller.
-        // Find the "Call Filter" row by its title TextView inside the list, then
-        // click the parent LinearLayout row to select it.
+        // Try finding the list using the AOSP package name
         UiObject listView = device.findObject(new UiSelector()
                 .resourceId("com.android.permissioncontroller:id/list"));
+
+        // If not found, try the Google package name (Common on API 30+ Play images)
+        if (!listView.waitForExists(1000)) {
+            listView = device.findObject(new UiSelector()
+                    .resourceId("com.google.android.permissioncontroller:id/list"));
+        }
+
+        // If still not found, try a generic class match (Last resort fallback)
+        if (!listView.exists()) {
+            listView = device.findObject(new UiSelector()
+                    .className("android.widget.ListView"));
+        }
+
         if (!listView.waitForExists(3000)) {
+            // Dialog didn't appear or we couldn't find it
             return;
         }
 
+        // Find the app by text "Call Filter" (Ensure your emulator is in English)
         UiObject callFilterTitle = listView.getChild(new UiSelector()
-                .resourceId("com.android.permissioncontroller:id/title")
-                .text("Call Filter"));
-        if (callFilterTitle.waitForExists(1000)) {
-            // Click the parent row to select the radio button
-            callFilterTitle.click();
+                .textContains("Call Filter"));
 
-            // "Set as default" (android:id/button1) becomes enabled after selection
-            UiObject setDefaultButton = device.findObject(new UiSelector()
-                    .resourceId("android:id/button1")
-                    .enabled(true));
-            if (setDefaultButton.waitForExists(2000)) {
-                setDefaultButton.click();
-            }
+        if (callFilterTitle.waitForExists(2000)) {
+            callFilterTitle.click();
+        }
+
+        // Click "Set as default"
+        // Note: The ID "android:id/button1" is standard and safe to use
+        UiObject setDefaultButton = device.findObject(new UiSelector()
+                .resourceId("android:id/button1")
+                .enabled(true));
+
+        if (setDefaultButton.waitForExists(2000)) {
+            setDefaultButton.click();
         }
     }
 }
