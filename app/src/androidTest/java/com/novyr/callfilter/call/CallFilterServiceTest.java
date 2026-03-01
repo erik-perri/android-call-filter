0c9ab9fb2f84bfd605838b4490957794bff26b34
Erik Perri <erik.perri@gmail.com>
2026-03-01 13:16:12 -0500

    test: update call filter test to check the same paths as screening test

--- app/src/androidTest/java/com/novyr/callfilter/call/CallFilterServiceTest.java
+++ app/src/androidTest/java/com/novyr/callfilter/call/CallFilterServiceTest.java
@@ -14,6 +14,7 @@ import com.novyr.callfilter.db.entity.enums.RuleType;
 import com.novyr.callfilter.ui.loglist.LogListActivity;
 import com.novyr.callfilter.util.ApiLevelAssumptions;
 import com.novyr.callfilter.util.CallSimulator;
+import com.novyr.callfilter.util.ContactHelper;
 import com.novyr.callfilter.util.DatabaseHelper;
 import com.novyr.callfilter.util.PermissionHelper;
 
@@ -41,6 +42,7 @@ public class CallFilterServiceTest {
             new ActivityScenarioRule<>(LogListActivity.class);
 
     private DatabaseHelper dbHelper;
+    private ContactHelper contactHelper;
 
     @Before
     public void setUp() throws Exception {
@@ -52,10 +54,14 @@ public class CallFilterServiceTest {
 
         dbHelper = new DatabaseHelper();
         dbHelper.clearLogs();
+        contactHelper = new ContactHelper();
     }
 
     @After
     public void tearDown() {
+        if (contactHelper != null) {
+            contactHelper.cleanupContacts();
+        }
         if (dbHelper != null) {
             dbHelper.clearLogs();
             dbHelper.resetRules(
@@ -115,7 +121,7 @@ public class CallFilterServiceTest {
     }
 
     @Test
-    public void allowed() throws Exception {
+    public void noMatchingRulesAllowed() throws Exception {
         dbHelper.resetRules(
                 new RuleEntity(RuleType.UNMATCHED, RuleAction.ALLOW, null, true, 0)
         );
@@ -127,6 +133,75 @@ public class CallFilterServiceTest {
             assertEquals(LogAction.ALLOWED, log.getAction());
         } finally {
             cancelCallSilently("5551234");
+        }
+    }
+
+    @Test
+    public void contactAllowed() throws Exception {
+        dbHelper.resetRules(
+                new RuleEntity(RuleType.UNRECOGNIZED, RuleAction.BLOCK, null, true, 4),
+                new RuleEntity(RuleType.UNMATCHED, RuleAction.ALLOW, null, true, 0)
+        );
+        contactHelper.insertContact("Test Contact", "5553456");
+
+        CallSimulator.simulateIncomingCall("5553456");
+        try {
+            LogEntity log = pollForLogEntry();
+            assertEquals("5553456", log.getNumber());
+            assertEquals(LogAction.ALLOWED, log.getAction());
+        } finally {
+            cancelCallSilently("5553456");
+        }
+    }
+
+    @Test
+    public void areaCodeBlocked() throws Exception {
+        dbHelper.resetRules(
+                new RuleEntity(RuleType.AREA_CODE, RuleAction.BLOCK, "800", true, 4),
+                new RuleEntity(RuleType.UNMATCHED, RuleAction.ALLOW, null, true, 0)
+        );
+
+        CallSimulator.simulateIncomingCall("8005551234");
+        try {
+            LogEntity log = pollForLogEntry();
+            assertEquals("8005551234", log.getNumber());
+            assertEquals(LogAction.BLOCKED, log.getAction());
+        } finally {
+            cancelCallSilently("8005551234");
+        }
+    }
+
+    @Test
+    public void patternBlocked() throws Exception {
+        dbHelper.resetRules(
+                new RuleEntity(RuleType.MATCH, RuleAction.BLOCK, "555*", true, 4),
+                new RuleEntity(RuleType.UNMATCHED, RuleAction.ALLOW, null, true, 0)
+        );
+
+        CallSimulator.simulateIncomingCall("5551234");
+        try {
+            LogEntity log = pollForLogEntry();
+            assertEquals("5551234", log.getNumber());
+            assertEquals(LogAction.BLOCKED, log.getAction());
+        } finally {
+            cancelCallSilently("5551234");
+        }
+    }
+
+    @Test
+    public void disabledRuleIgnored() throws Exception {
+        dbHelper.resetRules(
+                new RuleEntity(RuleType.UNRECOGNIZED, RuleAction.BLOCK, null, false, 4),
+                new RuleEntity(RuleType.UNMATCHED, RuleAction.ALLOW, null, true, 0)
+        );
+
+        CallSimulator.simulateIncomingCall("5559999");
+        try {
+            LogEntity log = pollForLogEntry();
+            assertEquals("5559999", log.getNumber());
+            assertEquals(LogAction.ALLOWED, log.getAction());
+        } finally {
+            cancelCallSilently("5559999");
         }
     }
 
