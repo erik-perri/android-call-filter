696ae70c88db155ad4c280357cfa7bb94d3f2966
Erik Perri <erik.perri@gmail.com>
2026-03-01 20:03:19 -0500

    test: merge rule checker tests

--- app/src/test/java/com/novyr/callfilter/RuleCheckerTest.java
+++ app/src/test/java/com/novyr/callfilter/RuleCheckerTest.java
@@ -1,5 +1,8 @@
 package com.novyr.callfilter;
 
+import android.os.Build;
+import android.telecom.Connection;
+
 import com.novyr.callfilter.db.entity.RuleEntity;
 import com.novyr.callfilter.db.entity.enums.RuleAction;
 import com.novyr.callfilter.db.entity.enums.RuleType;
@@ -9,6 +12,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -122,5 +126,126 @@ public class RuleCheckerTest {
         assertTrue(ruleChecker.allowCall(new CallDetails(null)));
         assertTrue(ruleChecker.allowCall(new CallDetails(UNRECOGNIZED_NUMBER)));
         assertTrue(ruleChecker.allowCall(new CallDetails(RECOGNIZED_NUMBER)));
+    }
+
+    @Test
+    public void allowCall_areaCode_blocked() {
+        RuleChecker ruleChecker = new RuleChecker(
+                new RuleHandlerManager(createFinderMock()),
+                new RuleEntity[]{
+                        new RuleEntity(RuleType.AREA_CODE, RuleAction.BLOCK, "800", true, 4),
+                        new RuleEntity(RuleType.UNMATCHED, RuleAction.ALLOW, null, true, 0),
+                }
+        );
+
+        assertFalse(ruleChecker.allowCall(new CallDetails("8005551234")));
+    }
+
+    @Test
+    public void allowCall_wildcardStar_blocked() {
+        RuleChecker ruleChecker = new RuleChecker(
+                new RuleHandlerManager(createFinderMock()),
+                new RuleEntity[]{
+                        new RuleEntity(RuleType.MATCH, RuleAction.BLOCK, "555*", true, 4),
+                        new RuleEntity(RuleType.UNMATCHED, RuleAction.ALLOW, null, true, 0),
+                }
+        );
+
+        assertFalse(ruleChecker.allowCall(new CallDetails("5551234")));
+    }
+
+    @Test
+    public void allowCall_exactMatch_blocked() {
+        RuleChecker ruleChecker = new RuleChecker(
+                new RuleHandlerManager(createFinderMock()),
+                new RuleEntity[]{
+                        new RuleEntity(RuleType.MATCH, RuleAction.BLOCK, "5551234", true, 4),
+                        new RuleEntity(RuleType.UNMATCHED, RuleAction.ALLOW, null, true, 0),
+                }
+        );
+
+        assertFalse(ruleChecker.allowCall(new CallDetails("5551234")));
+    }
+
+    @Test
+    public void allowCall_questionMarkMatch_blocked() {
+        RuleChecker ruleChecker = new RuleChecker(
+                new RuleHandlerManager(createFinderMock()),
+                new RuleEntity[]{
+                        new RuleEntity(RuleType.MATCH, RuleAction.BLOCK, "555????", true, 4),
+                        new RuleEntity(RuleType.UNMATCHED, RuleAction.ALLOW, null, true, 0),
+                }
+        );
+
+        assertFalse(ruleChecker.allowCall(new CallDetails("5551234")));
+    }
+
+    @Test
+    public void allowCall_questionMarkNoMatch_allowed() {
+        RuleChecker ruleChecker = new RuleChecker(
+                new RuleHandlerManager(createFinderMock()),
+                new RuleEntity[]{
+                        new RuleEntity(RuleType.MATCH, RuleAction.BLOCK, "555????", true, 4),
+                        new RuleEntity(RuleType.UNMATCHED, RuleAction.ALLOW, null, true, 0),
+                }
+        );
+
+        assertTrue(ruleChecker.allowCall(new CallDetails("55512345")));
+    }
+
+    @Test
+    public void allowCall_countryCodeNormalized_blocked() {
+        RuleChecker ruleChecker = new RuleChecker(
+                new RuleHandlerManager(createFinderMock()),
+                new RuleEntity[]{
+                        new RuleEntity(RuleType.MATCH, RuleAction.BLOCK, "5551234567", true, 4),
+                        new RuleEntity(RuleType.UNMATCHED, RuleAction.ALLOW, null, true, 0),
+                }
+        );
+
+        assertFalse(ruleChecker.allowCall(new CallDetails("15551234567")));
+    }
+
+    @Test
+    public void allowCall_verificationPassed_allowed() {
+        assumeTrue(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R);
+        RuleChecker ruleChecker = new RuleChecker(
+                new RuleHandlerManager(createFinderMock()),
+                new RuleEntity[]{
+                        new RuleEntity(RuleType.VERIFICATION_PASSED, RuleAction.ALLOW, null, true, 4),
+                        new RuleEntity(RuleType.UNMATCHED, RuleAction.BLOCK, null, true, 0),
+                }
+        );
+
+        assertTrue(ruleChecker.allowCall(new CallDetails("5551234", Connection.VERIFICATION_STATUS_PASSED)));
+    }
+
+    @Test
+    public void allowCall_verificationFailed_blocked() {
+        assumeTrue(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R);
+        RuleChecker ruleChecker = new RuleChecker(
+                new RuleHandlerManager(createFinderMock()),
+                new RuleEntity[]{
+                        new RuleEntity(RuleType.VERIFICATION_FAILED, RuleAction.BLOCK, null, true, 4),
+                        new RuleEntity(RuleType.UNMATCHED, RuleAction.ALLOW, null, true, 0),
+                }
+        );
+
+        assertFalse(ruleChecker.allowCall(new CallDetails("5551234", Connection.VERIFICATION_STATUS_FAILED)));
+    }
+
+    @Test
+    public void allowCall_verificationNotVerified_allowed() {
+        assumeTrue(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R);
+        RuleChecker ruleChecker = new RuleChecker(
+                new RuleHandlerManager(createFinderMock()),
+                new RuleEntity[]{
+                        new RuleEntity(RuleType.VERIFICATION_PASSED, RuleAction.ALLOW, null, true, 4),
+                        new RuleEntity(RuleType.VERIFICATION_FAILED, RuleAction.BLOCK, null, true, 2),
+                        new RuleEntity(RuleType.UNMATCHED, RuleAction.ALLOW, null, true, 0),
+                }
+        );
+
+        assertTrue(ruleChecker.allowCall(new CallDetails("5551234", Connection.VERIFICATION_STATUS_NOT_VERIFIED)));
     }
 }
